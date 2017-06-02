91e6074c9847901fcb3f4332b997c81070d972b2
Mark Pearl <mark.pearl@myob.com>
2017-06-02 16:20:11 +1200

    Add mocha tests

--- spec/Game.spec.js
+++ spec/Game.spec.js
@@ -1,3 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
-const Game = require('./game.js');
+const Game = require('../lib/game.js');
+
+describe('do something', function() {
+    it ('should pass', function() {
+        expect('bar').to.equal('bar');
+    });
+});
