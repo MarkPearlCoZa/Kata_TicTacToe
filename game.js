88ebb1944ba5befb7c6a429dfb6866fea33b1eb2
Mark Pearl <mark.pearl@myob.com>
2017-06-02 16:22:05 +1200

    Add mocha tests

---
+++ game.js
@@ -0,0 +1,13 @@
+function TicTacToe() {
+    this.baz = 'baz';
+}
+
+TicTacToe.prototype.start = function() {
+    
+}
+
+TicTacToe.prototype.hasWinner = function() {
+    return false;
+}
+
+module.exports = TicTacToe;
--- spec/Game.spec.js
+++ spec/Game.spec.js
@@ -2,8 +2,8 @@ const chai = require('chai');
 const expect = chai.expect;
 const Game = require('../lib/game.js');
 
-describe('do something', function() {
-    it ('should pass', function() {
+describe('New game', function() {
+    it ('should not have a winner', function() {
         expect('bar').to.equal('bar');
     });
 });
